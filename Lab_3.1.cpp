ebcc5c4b9baa2ed30f64f7eb589b34fe67785910
fxxwol <90700503+fxxwol@users.noreply.github.com>
2021-09-21 05:29:16 +0300

    Add files via upload

---
+++ Lab_3.1.cpp
@@ -0,0 +1,51 @@
+﻿// Lab_03_1.cpp
+// < Проців Роксолана >
+// Лабораторна робота № 3.1
+// Розгалуження, задане формулою: функція однієї змінної.
+// Варіант 20
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	double x;
+	double y;
+	double A;
+	double B;
+
+	cout << "x = "; cin >> x;
+
+	A = tan(x) + 1;
+
+	// спосіб 1: розгалуження в скороченій формі
+	if (x <= 5)
+	   B = exp(x / (1 - x));
+	if (-5 < x && x <= 3)
+	   B = 3.2 + log10(1.8 * x * x);
+	if (x > 3)
+	   B = abs(x) * sqrt(x);
+
+	y = A - B;
+
+	cout << endl;
+	cout << "1) y = " << y << endl;
+
+	// спосіб 2: розгалуження в повній формі
+	if (x <= 5)
+	    B = exp(x / (1 - x));
+	else
+		if (x > 3)
+	        B = abs(x) * sqrt(x);
+		else
+			B= 3.2 + log10(1.8 * x * x);
+
+	y = A - B;
+
+	cout << "2) y = " << y << endl;
+	cin.get();
+	return 0;
+}
+
